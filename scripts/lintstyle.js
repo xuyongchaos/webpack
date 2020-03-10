914143f9377461ea3a2fa32fffbe0f4b6b7b2d34
xuyongchao6585 <xuyongchao6585@ipalfish.com>
2020-03-10 21:03:26 +0800

    lintstyle

--- scripts/lintstyle.js
+++ scripts/lintstyle.js
@@ -2,8 +2,9 @@
 
 const { execSync } = require('child_process')
 const stylelint = require('stylelint')
+const path = require('path')
+const chalk = require('chalk')
 
-console.log('-------- lintstyle ---------')
 try {
   var ret = execSync('git diff --staged --diff-filter=ACMR --name-only', {
     encoding: 'utf-8'
@@ -15,10 +16,30 @@ try {
     .lint({
       files: files,
       syntax: 'less',
-      fix: true
+      fix: true,
+      configFile: path.join(__dirname, '../.stylelintrc.js')
     })
     .then(ret => {
-      console.log(ret.results)
+      if (ret.errored) {
+        const filterRets = ret.results.filter(x => x.errored)
+        filterRets.forEach(x => {
+          console.log(`source: ${x.source}`)
+          if (x.warnings.length > 0) {
+            x.warnings.forEach(warning => {
+              console.log(chalk.blue(`warnings:${JSON.stringify(warning)}`))
+            })
+          }
+          if (x.parseErrors.length > 0) {
+            x.parseErrors.forEach(error => {
+              console.log(chalk.red(`parseErrors:${JSON.stringify(error)}`))
+            })
+          }
+        })
+        process.exit(-1)
+      }
+    })
+    .catch(err => {
+      throw new Error(err)
     })
 } catch (err) {
   throw new Error(err)
