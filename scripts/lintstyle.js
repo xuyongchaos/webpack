c418e2ad74c2d9805136ee4dd0dc679e82790010
xuyongchao <xuyongchao6585@ipalfish.com>
2020-03-11 00:34:29 +0800

    添加ora显示loading状态

--- scripts/lintstyle.js
+++ scripts/lintstyle.js
@@ -4,6 +4,8 @@ const { execSync } = require('child_process')
 const stylelint = require('stylelint')
 const path = require('path')
 const chalk = require('chalk')
+const ora = require('ora')
+const spinner = ora('style格式化开始...').start()
 
 const log = console.log
 try {
@@ -12,38 +14,43 @@ try {
   })
   var files = ret.split('\n') || []
   files = files.filter(x => /\.(css|less|vue)$/.test(x))
-  console.log(files)
-  stylelint
-    .lint({
-      files: files,
-      syntax: 'less',
-      fix: true,
-      configFile: path.join(__dirname, '../.stylelintrc.js')
-    })
-    .then(ret => {
-      if (ret.errored) {
-        const filterRets = ret.results.filter(x => x.errored)
-        filterRets.forEach(x => {
-          log(chalk.white('\n----------------------------------'))
-          log(chalk.green(`source: ${x.source}`))
-          if (x.warnings.length > 0) {
-            x.warnings.forEach(warning => {
-              log(chalk.yellow(`warnings:${JSON.stringify(warning)}`))
-            })
-          }
-          if (x.parseErrors.length > 0) {
-            x.parseErrors.forEach(error => {
-              log(chalk.red(`parseErrors:${JSON.stringify(error)}`))
-            })
-          }
-        })
-        process.exit(128)
-      }
-    })
-    .catch(err => {
-      throw new Error(err)
-      process.exit(128);
-    })
+  if (files.length) {
+    stylelint
+      .lint({
+        files: files,
+        syntax: 'less',
+        fix: true,
+        configFile: path.join(__dirname, '../.stylelintrc.js')
+      })
+      .then(ret => {
+        if (ret.errored) {
+          const filterRets = ret.results.filter(x => x.errored)
+          filterRets.forEach(x => {
+            log(chalk.white('\n----------------------------------'))
+            log(chalk.green(`source: ${x.source}`))
+            if (x.warnings.length > 0) {
+              x.warnings.forEach(warning => {
+                log(chalk.yellow(`warnings:${JSON.stringify(warning)}`))
+              })
+            }
+            if (x.parseErrors.length > 0) {
+              x.parseErrors.forEach(error => {
+                log(chalk.red(`parseErrors:${JSON.stringify(error)}`))
+              })
+            }
+          })
+          process.exit(128)
+        }
+      })
+      .catch(err => {
+        throw new Error(err)
+      // process.exit(128)
+      }).finally(() => {
+        spinner.stop()
+      })
+  } else {
+    spinner.stop()
+  }
 } catch (err) {
   throw new Error(err)
 }
