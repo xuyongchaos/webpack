e19d3543c51c1a7c8a75aa9e5de798ba06fa3a31
xuyongchao6585 <xuyongchao6585@ipalfish.com>
2020-03-10 13:33:40 +0800

    watch

--- webpack.config.js
+++ webpack.config.js
@@ -37,12 +37,14 @@ const config = {
     enforceExtension: false,
     extensions: ['.js', '.vue', '.json']
   },
+  // 使用webpack-dev-server的时候默认打开
+  watch: true,
   watchOptions: {
     // 类似于节流， 500ms内的更改内造成的build将会合并成一次
-    aggregateTimeout: 300,
+    aggregateTimeout: 500,
     // 通过传递 true 开启 polling，或者指定毫秒为单位进行轮询。
     poll: 1000,
-    ignored: /node_modules|dist/
+    ignored: /node_modules|dist|.vscode/
   },
   module: {
     rules: [
